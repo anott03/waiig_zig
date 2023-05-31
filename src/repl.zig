8bad5d845c17e70e0eb9396519978d2daf32d5d1
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-05-31 14:58:52 -0400

    repl

--- src/repl.zig
+++ src/repl.zig
@@ -7,18 +7,21 @@ const PROMPT = ">> ";
 pub fn start() !void {
     const stdin = std.io.getStdIn().reader();
     var buf: [80]u8 = undefined;
-    if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |line| {
-        var l = lexer.Lexer.new(line);
-        var tok = l.next_token() catch {
-            std.debug.print("Error getting next token");
-            return;
-        };
-        while (tok != token.Token.EOF) {
-            std.debug.print("{?}\n", .{tok});
-            tok = l.next_token() catch {
+    while (true) {
+        std.debug.print("{s}", .{PROMPT});
+        if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |line| {
+            var l = lexer.Lexer.new(line);
+            var tok = l.next_token() catch {
                 std.debug.print("Error getting next token");
                 return;
             };
+            while (tok != token.Token.EOF) {
+                std.debug.print("{?}\n", .{tok});
+                tok = l.next_token() catch {
+                    std.debug.print("Error getting next token");
+                    return;
+                };
+            }
         }
     }
 }
